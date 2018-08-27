3a6867e679c74088053ce1e34ec8dbc1ba633101
Ethan <ehschutzman@wpi.edu>
2018-08-26 23:33:23 -0400

    maybe this will work?

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ var server = http.createServer(function (req, res) {
         case '/index.html':
             sendFile(res, 'index.html')
             break
-        case '/style.css':
-            sendFile(res, 'style.css')
-            break
         default:
             res.end('404 not found')
     }
