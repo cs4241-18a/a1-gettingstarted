26374ff4c3b3480dde5da0ba82cdaaf952d516db
Kyle Corry <kylecorry31@gmail.com>
2018-08-27 10:25:28 -0400

    Add live data

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http')
   , fs   = require('fs')
   , url  = require('url')
+  , overwatch = require('overwatch-api')
   , port = 8080;
+
+const platform = 'pc';
+const region = 'us';
+const tag = 'Synapse-11382';
 
 var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
@@ -14,10 +19,13 @@ var server = http.createServer (function (req, res) {
       sendFile(res, 'index.html')
       break
     case '/style.css':
-      sendFile(res, 'style.css')
+      sendFile(res, 'style.css', 'text/css')
       break
     case '/no-style.html':
       sendFile(res, 'no-style.html')
+      break
+    case '/overwatch':
+      getOverwatchProfile(res);
       break
     default:
       res.end('404 not found')
@@ -29,11 +37,24 @@ console.log('listening on 8080')
 
 // subroutines
 
-function sendFile(res, filename) {
-
+function sendFile(res, filename, type) {
+  type = type || 'text/html';
   fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
+    res.writeHead(200, {'Content-type': type})
     res.end(content, 'utf-8')
   })
 
+}
+
+
+function sendJSON(res, data){
+  res.writeHead(200, {'Content-type': 'application/json'})
+  res.end(JSON.stringify(data), 'utf-8')
+}
+
+function getOverwatchProfile(res){
+  overwatch.getProfile(platform, region, tag, (err, json) => {
+    if (err) console.error(err);
+    else sendJSON(res, json)
+  });
 }
