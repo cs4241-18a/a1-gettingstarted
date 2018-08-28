df77ebc412e2712b9a07b1fef7e8418f59c2ce0d
Kyle <kqsavell@wpi.edu>
2018-08-27 20:24:50 -0400

    css attempt 2

--- server.js
+++ server.js
@@ -4,32 +4,43 @@ var http = require('http')
   , port = 8080;
 
 var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
+  var uri = url.parse(req.url);
 
   switch( uri.pathname ) {
     case '/':
-      sendFile(res, 'index.html')
-      sendFile(res, 'styles.css')
-      break
+      sendFile(res, 'index.html');
+      sendFile(res, 'styles.css');
+      break;
     case '/index.html':
-      sendFile(res, 'index.html')
-      sendFile(res, 'styles.css')
-      break
+      sendFile(res, 'index.html');
+      sendFile(res, 'styles.css');
+      break;
     default:
       res.end('404 not found')
   }
 })
 
 server.listen(process.env.PORT || port);
-console.log('listening on 8080')
+console.log('listening on 8080');
 
 // subroutines
 
 function sendFile(res, filename) {
 
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
+  switch(filename) {
+    default:
+    case 'index.html':
+      fs.readFile(filename, function (error, content) {
+              res.writeHead(200, {'Content-type': 'text/html'});
+              res.end(content, 'utf-8');
+          });
+      break;
+    case 'styles.css':
+      fs.readFile(filename, function (error, content) {
+          res.writeHead(200, {'Content-type': 'text/css'});
+          res.end(content, 'utf-8');
+      });
+      break;
+  }
 
 }
