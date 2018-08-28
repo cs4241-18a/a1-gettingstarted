c1723b40d5ef5d0b553ebbbaa9f9f3f2d30c27d9
Sourec <tothemyers@gmail.com>
2018-08-28 14:07:21 -0400

    Populate main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTooltipModule, MatCardModule, MatSnackBarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,10 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule,
+    MatCardModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material';
 import { map } from 'rxjs/operators';
 
 @Component({
@@ -14,7 +15,14 @@ export class NavComponent {
     .pipe(
       map(result => result.matches)
     );
-    
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  
+
+  constructor(private breakpointObserver: BreakpointObserver, private snackBar: MatSnackBar) {}
+
+  dontDoThat(): void {
+    this.snackBar.open('Why would you click on that? What\'s wrong with you?', 'Don\'t ask', { duration: 2500 });
   }
+
+  cantClick(): void {
+    this.snackBar.open('Can\'t click on me!', 'Yes I can!', { duration: 50 });
+  }
+}
