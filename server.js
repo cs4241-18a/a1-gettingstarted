ee3c6e33dc96c05659d83827e04e6af730dd99a8
Kamil Gumienny <kamilgummy@outlook.com>
2018-08-29 18:47:12 -0400

    Finalized everything but experience list

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-var http = require('http')
-  , fs   = require('fs')
-  , url  = require('url')
-  , port = 8080;
+var http = require('http'),
+  fs = require('fs'),
+  url = require('url'),
+  port = 8080;
 
-var server = http.createServer (function (req, res) {
+var server = http.createServer(function(req, res) {
   var uri = url.parse(req.url)
 
-  switch( uri.pathname ) {
+  switch (uri.pathname) {
     case '/':
       sendFile(res, 'index.html')
       break
     case '/index.html':
       sendFile(res, 'index.html')
+      break
+    case '/styles.css':
+      sendFile(res, 'styles.css')
+      break
+    case '/kubus.jpg':
+      sendFile(res, 'kubus.jpg')
+      break
+    case '/background.jpg':
+      sendFile(res, 'background.jpg')
+      break
+    case '/gym.jpg':
+      sendFile(res, 'gym.jpg')
+      break
+    case '/music.jpg':
+      sendFile(res, 'music.jpg')
+      break
+    case '/thetachi.jpg':
+      sendFile(res, 'thetachi.jpg')
       break
     default:
       res.end('404 not found')
