5b9007c3e9e5145224f851f3d16d17e57c998583
Nate Carter <nhcarter@wpi.edu>
2018-08-29 12:43:40 -0400

    actually fixed it this time

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http')
   , url  = require('url')
   , port = 8080;
 
-var server = http.createServer (function (req, res)) {
+var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
 
   switch( uri.pathname ) {
@@ -13,13 +13,13 @@ var server = http.createServer (function (req, res)) {
     case '/index.html':
       sendFile(res, 'index.html', 1)
       break
-	  case '/styles.css':
+	case '/styles.css':
       sendFile(res, 'styles.css', 0)
       break
     default:
       res.end('404 not found')
   }
-}
+})
 
 server.listen(process.env.PORT || port);
 console.log('listening on 8080')
@@ -34,6 +34,5 @@ function sendFile(res, filename, type) {
 		res.writeHead(200, {'Content-type': 'text/css'})
 	}
     res.end(content, 'utf-8')
-  }
-
+  })
 }
