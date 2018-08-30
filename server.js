626e5df8d2d52edcd5b5218531e04808f69029b8
Madeline Burke <mburke@wpi.edu>
2018-08-30 15:43:17 -0400

    organized into containers

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var server = http.createServer (function (req, res) {
     case '/bootstrap.css':
       sendFile(res, 'bootstrap.css')
       break
-    case 'prof.jpg':
+    case '/prof.jpg':
       sendFile(res, 'prof.jpg')
       break
     default:
