b602e2a3d0f3e28000027150088f432d9879641f
skyler <konata.izumi20@gmail.com>
2018-08-30 00:00:29 -0400

    added ability to change css with clicking footer image

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function changeMode() {
+    var footerImage = document.getElementById("footerImage")
+    var isDarkMode = footerImage.getAttribute("value");
+    if(isDarkMode == 0) {
+        footerImage.setAttribute("value",1);
+        document.getElementById("mode").href = "style/dark.css"
+        console.log("switch to dark");
+    } else {
+        footerImage.setAttribute("value",0);
+        document.getElementById("mode").href = "style/light.css"
+        console.log("switch to light");
+    }
+}
--- server.js
+++ server.js
@@ -13,8 +13,23 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
-    case '/style.css':
-      sendCSS(res,'style.css')
+    case '/style/dark.css':
+      sendCSS(res,'style/dark.css')
+      break
+    case '/style/light.css':
+      sendCSS(res,'style/light.css')
+      break
+    case '/style/main.css':
+      sendCSS(res,'style/main.css')
+      break
+    case '/img/umi_cry.png':
+      sendImg(res,'img/umi_cry.png')
+      break;
+    case '/img/mimikyu.png':
+      sendImg(res,'img/mimikyu.png')
+      break;
+    case '/scripts/main.js':
+      sendFile(res, 'scripts/main.js')
       break
     default:
       res.end('404 not found')
@@ -38,6 +53,14 @@ function sendFile(res, filename) {
 function sendCSS(res,filename) {
   fs.readFile(filename, function(error, content) {
     res.writeHead(200, {'Content-type': 'text/css'})
+    res.write(content)
+    res.end()
+  })
+}
+
+function sendImg(res,filename) {
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': 'image/png'})
     res.write(content)
     res.end()
   })
