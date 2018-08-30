9f4681a76f42da9067b5d914ee935c002542c621
Maggie Goodwin <magoodwin@wpi.edu>
2018-08-30 18:59:58 -0400

    added additional image, fixed sendFile to accept different content types, added Google fonts, centered site on browser

--- server.js
+++ server.js
@@ -8,16 +8,16 @@ var server = http.createServer (function (req, res) {
 
   switch( uri.pathname ) {
     case '/':
-      sendFile(res, 'index.html')
+      sendFile(res, 'index.html','text/html')
       break
     case '/index.html':
-      sendFile(res, 'index.html')
+      sendFile(res, 'index.html','text/html')
       break
 	case '/styles.css':
-      sendFile(res, 'styles.css')
+      sendFile(res, 'styles.css','text/css')
       break
 	case '/AGD_image.png':
-	  sendFile(res, 'AGD_image.png')
+	  sendFile(res, 'AGD_image.png', 'image/png')
 	  break
     default:
       res.end('404 not found')
@@ -29,10 +29,10 @@ console.log('listening on 8080')
 
 // subroutines
 
-function sendFile(res, filename) {
+function sendFile(res, filename, ct) {
 
   fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
+    res.writeHead(200, {'Content-type': ct})
     res.end(content, 'utf-8')
   })
 
