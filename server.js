7917e1c8d8d30ecb5b5e7583afbee0aeda033e9e
Abigail Roane <arroane@wpi.edu>
2018-08-30 16:13:38 -0400

    Add further design and technical achievements

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
   , fs   = require('fs')
   , url  = require('url')
-  , port = 8080;
+  , port = 3000;
 
 var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
@@ -13,13 +13,16 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+    case '/style.css':
+      sendFile(res, 'style.css')
+      break
     default:
       res.end('404 not found')
   }
 })
 
 server.listen(process.env.PORT || port);
-console.log('listening on 8080')
+console.log('listening on 3000')
 
 // subroutines
 
