95036c3d1891d6488924b2f200400c1724a4a9a5
J3r3my97 <jerryxu1997@hotmail.com>
2018-08-30 10:52:44 -0400

    update3

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
-    case '/IMG_5771.jpg':
-      break
     case '/background':
       case
     default:
