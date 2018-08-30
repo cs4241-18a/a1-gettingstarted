162847f2de897f6a90e8d8cfccdf85fe35dbb90c
aantaya1 <antayaalexander@gmail.com>
2018-08-29 23:24:52 -0400

    I did a lottttt of dev in another repo... but it turned out i forgot to fork it from master (rather i just cloned it).... so i needed to make a new repo with none of my VC history so I could make this pull request and submit

--- server.js
+++ server.js
@@ -13,6 +13,33 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+    case '/experience.html':
+      sendFile(res, 'experience.html')
+      break
+    case '/projects.html':
+      sendFile(res, 'projects.html')
+      break
+    case '/about_me.html':
+      sendFile(res, 'about_me.html')
+      break
+    case '/contact.html':
+      sendFile(res, 'contact.html')
+      break
+    case '/envelope.png':
+      sendFile(res, 'envelope.png')
+      break
+    case '/linkedin.png':
+      sendFile(res, 'linkedin.png')
+      break
+    case '/meMachuPicchu.jpg':
+      sendFile(res, 'meMachuPicchu.jpg')
+      break
+    case '/Professional4.jpg':
+      sendFile(res, 'Professional4.jpg')
+      break
+    case '/main.css':
+      sendCssFile(res, 'main.css')
+      break
     default:
       res.end('404 not found')
   }
@@ -24,10 +51,15 @@ console.log('listening on 8080')
 // subroutines
 
 function sendFile(res, filename) {
-
   fs.readFile(filename, function(error, content) {
     res.writeHead(200, {'Content-type': 'text/html'})
     res.end(content, 'utf-8')
   })
+}
 
+function sendCssFile(res, filename) {
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(200, {'Content-type': 'text/css'})
+    res.end(content, 'utf-8')
+  })
 }
