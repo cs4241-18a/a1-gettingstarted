9df044ca2db73864b7767e636be02c5034cc5f9c
wittenber0 <ryanwwittenberg@gmail.com>
2018-08-30 15:40:58 -0400

    adding css and js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+    case '/style.css':
+      sendFile(res, 'style.css')
+      break
+    case '/ethereumInfo.js':
+      sendFile(res, 'ethereumInfo.js')
+      break
     default:
       res.end('404 not found')
   }
