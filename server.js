f67aea28f6442d215d01faf4611e7ae2026be72e
Krysta <krmurdy@wpi.edu>
2018-08-30 16:06:09 -0400

    Fixed a couple errors with loading the pictures and stylesheet

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+    case '/stylesA1.css':
+      sendFile(res, 'stylesA1.css')
+      break
+    case '/MoLProj.JPG':
+        sendFile(res, 'MoLProj.JPG')
+        break
+    case '/kittyCollage.JPG':
+        sendFile(res, 'kittyCollage.JPG')
+        break
+    case '/PointCloudBasic.JPG':
+        sendFile(res, 'PointCloudBasic.JPG')
+        break
+    case '/Skydiving.JPG':
+        sendFile(res, 'Skydiving.JPG')
+        break
+    case '/SwissAlps.JPG':
+        sendFile(res, 'SwissAlps.JPG')
+        break
     default:
       res.end('404 not found')
   }
@@ -24,10 +42,33 @@ console.log('listening on 8080')
 // subroutines
 
 function sendFile(res, filename) {
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
-
+    switch( filename ) {
+        case 'index.html':
+            fs.readFile(filename, function (error, content) {
+                res.writeHead(200, { 'Content-type': 'text/html' })
+                res.end(content, 'utf-8')
+            })
+            break
+        case 'stylesA1.css':
+            fs.readFile(filename, function (error, content) {
+                res.writeHead(200, { 'Content-type': 'text/css' })
+                res.end(content, 'utf-8')
+            })
+            break
+        case 'MoLProj.JPG':
+        case 'kittyCollage.JPG':
+        case 'PointCloudBasic.JPG':
+        case 'Skydiving.JPG':
+        case 'SwissAlps.JPG':
+            fs.readFile(filename, function (error, content) {
+                res.writeHead(200, { 'Content-type': 'image/jpg' })
+                res.end(content, 'utf-8')
+            })
+            break
+        default:
+            fs.readFile(filename, function (error, content) {
+                res.writeHead(200, { 'Content-type': 'text/html' })
+                res.end(content, 'utf-8')
+            })
+    }
 }
