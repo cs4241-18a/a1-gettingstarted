5faa0a341845a0908901f8b69e1d519d344fdf0e
skyler <konata.izumi20@gmail.com>
2018-08-30 15:41:21 -0400

    added saving style choice in localstorage

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 function changeMode() {
-    var footerImage = document.getElementById("footerImage")
-    var isDarkMode = footerImage.getAttribute("value");
+    var isDarkMode = localStorage.getItem("isDarkMode");
     if(isDarkMode == 0) {
-        footerImage.setAttribute("value",1);
-        document.getElementById("mode").href = "style/dark.css"
+        document.getElementById("mode").href = "style/dark.css";
+        localStorage.setItem("isDarkMode",1);
         console.log("switch to dark");
     } else {
-        footerImage.setAttribute("value",0);
-        document.getElementById("mode").href = "style/light.css"
+        document.getElementById("mode").href = "style/light.css";
+        localStorage.setItem("isDarkMode",0);
         console.log("switch to light");
+    }
+}
+
+function initMode() {
+    if(localStorage.getItem("isDarkMode") == undefined) {
+        localStorage.setItem("isDarkMode",0);
+    }
+    var isDarkMode = localStorage.getItem("isDarkMode");
+    if(isDarkMode == 0) {
+        document.getElementById("mode").href = "style/light.css";
+    } else {
+        document.getElementById("mode").href = "style/dark.css";
     }
 }
