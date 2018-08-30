787d106de04dd4c8c7948f426592287fc99a1b01
Yuanda(Danny) Song <ysong5@wpi.edu>
2018-08-29 23:56:59 -0400

    added css

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ var server = http.createServer (function (req, res) {
     case '/index.html':
       sendFile(res, 'index.html')
       break
+      case '/css/styles.css':
+      sendFile(res, 'styles.css')
+      break
     default:
       res.end('404 not found')
   }
