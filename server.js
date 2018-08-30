3c94f95c19bbbda6ba80d1ad76106664ddf08517
Cameron Maitland <e1ngosuosu@gmail.com>
2018-08-30 19:04:22 -0400

    test

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ var server = http.createServer (function (req, res) {
 
   switch( uri.pathname ) {
     case '/':
-      sendFile(res, 'index.html')
       sendStyle(res, 'style.css')
+      sendFile(res, 'index.html')
       break
     case '/index.html':
-      sendFile(res, 'index.html')
       sendStyle(res, 'style.css')
+      sendFile(res, 'index.html')
       break
     default:
       res.end('404 not found')
