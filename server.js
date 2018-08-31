d1246ca6c236c4154d2542afb31ceca773ea6464
Tarek Safa <tarek1000100@gmail.com>
2018-08-30 21:26:32 -0400

    last last commit

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ var server = http.createServer (function (req, res) {
     case '/styles.css':
       sendFile(res, 'styles.css')
       break
-    case '/headshot.jpg':
-      sendFile(res, '/headshot.jpg')
+    case 'headshot.jpg':
+      sendFile(res, 'headshot.jpg')
       break
     default:
       res.end('404 not found')
