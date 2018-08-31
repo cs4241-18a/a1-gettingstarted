f27d6f109df7cc93af07712b2c3db756a44a0d2c
jtfakult <jtfakult@wpi.edu>
2018-08-30 21:45:43 -0400

    Did basically everything else...

--- README.md
+++ README.md
@@ -4,11 +4,20 @@ Assignment 1 - Hello World: Basic Deployment w/ Git, GitHub, Heroku
 Jacob Fakult
 http://a1-jtfakult.herokuapp.com
 
-This project shows ...
+This project shows a basic website advertising a few things about myself... ooh la la!
+
+NOTE!!! This project is best viewed in the latest version of Chrome or Safari.
 
 ## Technical Achievements
+Rewrote sendFile() function in server.js in order to automatically return the requested file path.
+Rewrote sendFile() function in server.js in order to handle 404 errors.
+Added CSS, JS, and images to their own respective directories.
+Implemented a Javascript library called SweetAlert to give the user some extra information.
+Added comments to ensure code readability.
 
 
 ### Design Achievements
-
-
+Used CSS and animations to make the site sparkly.
+Added a nice 404 page (much code taken from other sites and previous projects of my own).
+Used SweetAlert to help the user navigate the webpage.
+Added arrow key control to improve navigability
--- server.js
+++ server.js
@@ -4,21 +4,15 @@ var http = require('http')
   , port = 8080;
 
 var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
+    var uri = url.parse(req.url)
 
-  switch( uri.pathname ) {
-    case '/':
-      sendFile(res, 'index.html')
-      break
-    case '/index.html':
-      sendFile(res, 'index.html')
-      break
-    case '/css/main.css':
-      sendFile(res, 'css/main.css')
-      break
-    default:
-      res.end('404 not found')
-  }
+	//console.log(uri.pathname + "--")
+	var path = uri.pathname
+	if (path[0] == '/')
+	{
+		path = path.substr(1)
+	}
+    sendFile(res, path)
 })
 
 server.listen(process.env.PORT || port);
@@ -26,16 +20,34 @@ console.log('listening on 8080')
 
 // subroutines
 
-function sendFile(res, filename) {
-
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(content, 'utf-8')
-  })
-  
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/css'})
-    res.end(content, 'utf-8')
-  })
-
+function sendFile(res, filename)
+{	
+	if (filename.length == 0)
+	{
+		filename = "index.html"
+	}
+	
+	try
+	{
+		//console.log(filename + "--")
+	    var stats = fs.statSync(filename); //Checks for file existence or errors
+		
+	    // File exists
+	  
+		var type = filename.split(".")
+		type = type[type.length-1]
+		fs.readFile(filename, function(error, content) {
+			res.writeHead(200, {'Content-type': 'text/' + type } )
+			res.end(content, 'utf-8')
+		})
+	}
+	catch(err)
+	{
+	    // File does not exist
+		
+		fs.readFile("404.html", function(error, content) {
+			res.writeHead(404, {'Content-type': 'text/html'})
+			res.end(content, 'utf-8')
+		})
+	}
 }
